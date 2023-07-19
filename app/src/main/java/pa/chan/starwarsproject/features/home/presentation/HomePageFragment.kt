e67d248588085f20eefe6fa24fef7aecda07dae4
pogch <pogchamb1@gmail.com>
2023-07-19 16:07:14 +0300

    add view for home page

---
+++ app/src/main/java/pa/chan/starwarsproject/features/home/presentation/HomePageFragment.kt
@@ -0,0 +1,19 @@
+package pa.chan.starwarsproject.features.home.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pa.chan.starwarsproject.R
+
+class HomePageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home_page, container, false)
+    }
+
+}
