5b413fb5b816a2eead6132901806240a16f28264
pogch <pogchamb1@gmail.com>
2023-07-19 15:17:52 +0300

    add di for home page and app

---
+++ app/src/main/java/pa/chan/starwarsproject/features/core/App.kt
@@ -0,0 +1,8 @@
+package pa.chan.starwarsproject.features.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+}
--- app/src/main/java/pa/chan/starwarsproject/features/core/MainActivity.kt
+++ app/src/main/java/pa/chan/starwarsproject/features/core/MainActivity.kt
@@ -1,8 +1,11 @@
-package pa.chan.starwarsproject
+package pa.chan.starwarsproject.features.core
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import pa.chan.starwarsproject.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
